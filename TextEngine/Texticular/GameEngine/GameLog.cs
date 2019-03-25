93ef285faca58798c15676518b97d1c98096c4a3
MegaMane <47061156+MegaMane@users.noreply.github.com>
2019-03-25 13:08:06 -0700

    added game log to replace static input response on game controller

---
+++ TextEngine/Texticular/GameEngine/GameLog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Texticular.GameEngine
+{
+    class GameLog
+    {
+        private static GameLog _instance;
+        public static StringBuilder InputResponse { get; set; }
+        private static List<string> _logText;
+
+        private GameLog()
+        {
+            InputResponse = new StringBuilder();
+            _logText = new List<string>();
+        }
+
+        public string DisplayResponse()
+        {
+            var outputString = InputResponse.ToString();
+            _logText.Add(outputString);
+            InputResponse.Clear();
+            return outputString;
+        }
+
+        public static GameLog Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new GameLog();
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
