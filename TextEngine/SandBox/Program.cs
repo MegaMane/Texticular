112f146413dcb6437f02c4c182b750ffff30d7c9
Jon Ludena <eljonnyrage@gmail.com>
2019-04-06 01:17:39 -0700

    Method for finding the correct object
    To Do: Add Adjective list to GameObject Class

--- TextEngine/SandBox/Program.cs
+++ TextEngine/SandBox/Program.cs
@@ -15,7 +15,7 @@ namespace SandBox
                 {
                     new GameObject()
                     {
-
+                        ID=1,
                         Name="Mug",
                         Description="A red ceramic mug that looks like the Marvel Villain Carnage",
                         Adjectives=new List<string>() {"Carnage"}
@@ -25,40 +25,171 @@ namespace SandBox
                 {
                     new GameObject()
                     {
-
+                        ID=2,
                         Name="Mug",
                         Description="A faded yellow mug with a small chip in the side.",
+                        Adjectives=new List<string>() {"Faded", "Yellow"}
+
+                    }
+                },
+                {
+                    new GameObject()
+                    {
+                        ID=3,
+                        Name="Duck",
+                        Description="A rubber duck. It looks like a duck.",
                         Adjectives=new List<string>() {"Yellow"}
 
                     }
-                }
+                },
+
+                {
+                    new GameObject()
+                    {
+                        ID=4,
+                        Name="Money",
+                        Description="50 green backs.",
+
+                    }
+                },
+
+                {
+                    new GameObject()
+                    {
+                        ID=5,
+                        Name="Monkey",
+                        Description="A monkey who cray.",
+                        Adjectives=new List<string>() {"Crazy"}
+                    }
+                },
+
+                {
+                    new GameObject()
+                    {
+                        ID=6,
+                        Name="Monk",
+                        Description="A warrior monk who smelled one too many gonesh sticks.",
+                        Adjectives=new List<string>() {"Crazy"}
+                    }
+                },
+                 {
+                    new GameObject()
+                    {
+                        ID=7,
+                        Name="Duck",
+                        Description="Just like the yellow duck, but uglier.",
+                        Adjectives=new List<string>() {"Ugly","Yellow"}
+
+                    }
+                },
             };
-          
-            foreach (var item in nouns)
+
+            var input = "Monkey";
+
+            List<GameObject> foundObjects = FindObject(input, nouns);
+
+            if(foundObjects.Count == 0)
             {
-                Console.WriteLine($"{String.Join(" ",item.Adjectives)} {item.Name}: {item.Description}\n");
+                Console.WriteLine($"{input} is not a sentence I understand.");
             }
+
+            foreach (var item in foundObjects)
+            {
+                Console.WriteLine($"{String.Join(" ", item.Adjectives)} {item.Name}: {item.Description}".Trim() + "\n");
+            }
+
 
             Console.ReadKey();
         }
 
 
-        public static string FindObject(string input)
+        //to make a match you must match 0 or more adjectives in any order plus the direct objectName
+        //-or-
+        //1 or more adjectives with an optional direct object name
+        public static List<GameObject> FindObject(string input, List<GameObject> nouns)
         {
-            input = input.ToLower();
-            string results = "";
-            List<string> nouns = new List<string> { "carnage mug", "yellow mug", "yellow duck", "yellow dumbwaiter", "money", "crazy monkey", "crazy monk" };
-            var filtered = nouns.Where(s1 => s1.Contains(input)).ToList(); ;
+            var results = new List<GameObject>();
+            
+            input = input.ToLower().Trim();
+            var words = input.Split();
+            List<GameObject> possibleMatches = new List<GameObject>();
+            
 
-            foreach (var gameObject in filtered)
+            //compile a list of any objects that have at least one matching word with the input string
+            foreach (var word in words)
             {
-                var search = input.Split(new char[] { ' ' });
-                var words = gameObject.Split(new char[] { ' ' });
-                //var foundObjects = search.Where(word => words.Any( w => w.Equals(word))).ToList();
-                var foundObjects = words.Where(word => search.Any(sWord => sWord.Equals(word))).ToList();
-                if (foundObjects.Count > 0)
+             var filtered = nouns.Where(n => n.Name.ToLower().Contains(word) || n.Adjectives.Any(a => a.ToLower().Contains(word)));
+             possibleMatches.AddRange(filtered);
+            }
+
+            //remove any duplicates by checking the object ID property 
+            possibleMatches = possibleMatches.Distinct(new GameObjectComparer()).ToList();
+
+            if(possibleMatches.Count == 0)
+            {
+                return results;
+            }
+
+            foreach (var gameObject in possibleMatches)
+            {
+                List<string> adjectives = new List<string>();
+                List<string> unmatchedWords = new List<string>(words);
+                bool directObjectFound = false;
+                bool matchFound = false;
+
+                //check for the name of the object
+                for (int i =0; i< unmatchedWords.Count; i++)
                 {
-                    var foundObject = String.Join(" ", words);
+
+                    if (unmatchedWords[i] == gameObject.Name.ToLower())
+                    {
+                        unmatchedWords.RemoveAt(i);
+                        i--;
+                        directObjectFound = true;
+                    }
+
+                }
+
+                //check if any adjectives match
+                for (int i = 0; i < unmatchedWords.Count; i++)
+                {
+                    for (int j = 0; j < gameObject.Adjectives.Count; j++)
+                    {
+                        if (unmatchedWords[i] == gameObject.Adjectives[j].ToLower())
+                        {
+                            adjectives.Add(unmatchedWords[i]);
+                            unmatchedWords.RemoveAt(i);
+                            i--;
+                            break;
+                        }
+
+                    }
+
+                }
+
+
+                if (unmatchedWords.Count == 0)
+                {
+                    if(directObjectFound)
+                    {
+                        //make sure that it's the last word
+                        matchFound = words[words.Length - 1] == gameObject.Name.ToLower();
+                    }
+
+                    else
+                    {
+                        matchFound = adjectives.Count > 0;
+                    }
+                }
+
+                else
+                {
+                    matchFound = false;
+                }
+
+                if (matchFound)
+                {
+                    results.Add(gameObject);
                 }
 
             }
@@ -74,13 +205,33 @@ namespace SandBox
     public class GameObject
     {
 
-
+        public int ID { get; set; }
         public string Name { get; set; }
         public List<string> Adjectives { get; set; }
         public string Description { get; set; }
 
+        public GameObject()
+        {
+            Adjectives = new List<string>();
+        }
 
+    }
 
+    internal class GameObjectComparer : IEqualityComparer<GameObject>
+    {
+        public bool Equals(GameObject x, GameObject y)
+        {
+            if (x.ID == y.ID)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int GetHashCode(GameObject obj)
+        {
+            return obj.Name.GetHashCode();
+        }
     }
 
 }
@@ -154,7 +305,4 @@ namespace SandBox
 
 //for a potential match
 
-//to make a match you must match 0 or more adjectives in any order plus the direct objectName
-//-or-
-//1 or more adjectives with an optional direct object name
 
